651c448f1dd31f1ab4e79eea9b77c4578833abdf
Trisha <trishablewis@gmail.com>
2017-11-08 14:22:37 -0500

    updated travis ci

--- spec/features/check_upload_spec.rb
+++ spec/features/check_upload_spec.rb
@@ -8,28 +8,10 @@ describe "check for successful data upload" do
 
   it "checks that the file has been uploaded" do
     login_as(@owner, :scope => :user)
-    puts @owner.login
-
-
-    DocumentUpload.all.each do |upload|
-      puts upload.file
-      puts upload.status
-    end
-
-    Collection.all.each do |c|
-      puts c.title
-      c.works.each do |w|
-        puts w.title
-      end
-    end
-
-
-=begin
     @work = Work.find_by(title: 'test')
     visit collection_read_work_path(@work.collection.owner, @work.collection, @work)
     expect(page).to have_content(@work.title)
     expect(page).to have_content(@work.pages.first.title)
-=end
   end
 
 end
