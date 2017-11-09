2489c4cae2e05bb2348c3ed076f44b9881e56542
Trisha <trishablewis@gmail.com>
2017-11-09 08:47:22 -0500

    testing branch

--- spec/features/check_upload_spec.rb
+++ spec/features/check_upload_spec.rb
@@ -8,7 +8,6 @@ describe "check for successful data upload" do
 
   it "checks that the file has been uploaded" do
     login_as(@owner, :scope => :user)
-    sleep(60)
     @work = Work.find_by(title: 'test')
     visit collection_read_work_path(@work.collection.owner, @work.collection, @work)
     expect(page).to have_content(@work.title)
