4e97940d8972ade681cd370dee3c765f6852d840
Trisha <trishablewis@gmail.com>
2017-11-15 13:17:31 -0500

    test git diff

--- travis/.travis_pry.sh
+++ travis/.travis_pry.sh
@@ -2,16 +2,14 @@
 echo "Testing for binding.pry"
 
 if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
-  echo "Pull request"
   TEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
 else
-  echo "Push on branch"
   TEST_BRANCH=$TRAVIS_BRANCH
 fi
 
-echo $TEST_BRANCH
+echo "Testing $TEST_BRANCH"
 
-DIFF_SEARCH=$(git diff --name-only $TEST_BRANCH $AGAINST -G "binding\.pry")
+DIFF_SEARCH=$(git diff --name-only $TEST_BRANCH HEAD^ $AGAINST -G "binding\.pry")
 
 echo $DIFF_SEARCH
 
