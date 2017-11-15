8b4d6c1562bf3ee61b128d199cc8da4c6a8dfc1d
Trisha <trishablewis@gmail.com>
2017-11-15 12:30:14 -0500

    updated travis

--- travis/.travis_pry.sh
+++ travis/.travis_pry.sh
@@ -1,7 +1,17 @@
 #hook to prevent binding.pry getting into production code
 echo "Testing for binding.pry"
 
-DIFF_SEARCH=$(git diff --name-only $TRAVIS_COMMIT_RANGE $AGAINST -G "binding\.pry")
+if [ $TRAVIS_PULL_REQUEST ]; then
+  echo "Pull request"
+  TEST_BRANCH = $TRAVIS_PULL_REQUEST_BRANCH
+else
+  echo "Push on branch"
+  TEST_BRANCH = $TRAVIS_BRANCH
+fi
+
+echo TEST_BRANCH
+
+DIFF_SEARCH=$(git diff --name-only  $AGAINST -G "binding\.pry")
 
 echo $DIFF_SEARCH
 
@@ -11,4 +21,4 @@ if [ "$DIFF_SEARCH" ]; then
   echo
 fi
 
-  exit 1
+exit 1
