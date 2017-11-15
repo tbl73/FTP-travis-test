b708415956a55dd60b5b595519d4a25c1f755e66
Trisha <trishablewis@gmail.com>
2017-11-15 09:58:44 -0500

    test

--- travis/.travis_pry.sh
+++ travis/.travis_pry.sh
@@ -3,13 +3,12 @@
 git diff --name-only HEAD^
 echo "Testing"
 
-DIFF_SEARCH=$(git diff --name-only $AGAINST -G "binding\.pry")
+DIFF_SEARCH=$(git diff --name-only HEAD^ $AGAINST -G "binding\.pry")
 
 echo $DIFF_SEARCH
 
 if [ "$DIFF_SEARCH" ]; then
   echo "Found binding.pry in these files:"
-  echo
   printf "$DIFF_SEARCH"
   echo
 
