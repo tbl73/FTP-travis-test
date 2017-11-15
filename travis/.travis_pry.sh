908a45f7f75464e35882143b2711f1020cbf4136
Trisha <trishablewis@gmail.com>
2017-11-15 10:45:07 -0500

    removed pry

--- travis/.travis_pry.sh
+++ travis/.travis_pry.sh
@@ -1,18 +1,6 @@
 #hook to prevent binding.pry getting into production code
 echo "Testing for binding.pry"
 
-echo "head to travis branch diff"
-
-TEST1=$(git diff --name-only HEAD...$TRAVIS_BRANCH $AGAINST -G "binding\.pry")
-
-echo $TEST1
-
-echo "head to travis branch diff with filter"
-TEST2=$(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH $AGAINST -G "binding\.pry")
-
-echo $TEST2
-echo "original search"
-
 DIFF_SEARCH=$(git diff --name-only $TRAVIS_COMMIT_RANGE $AGAINST -G "binding\.pry")
 
 echo $DIFF_SEARCH
