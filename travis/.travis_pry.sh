bca1097f317f92e5371510b6ec20356a22937723
Trisha <trishablewis@gmail.com>
2017-11-16 12:14:36 -0500

    binding.pry

--- travis/.travis_pry.sh
+++ travis/.travis_pry.sh
@@ -11,9 +11,9 @@ echo "Testing $TEST_BRANCH"
 
 DIFF_SEARCH=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
 
-PATTERN='[^#]binding\.pry'
+PATTERN='binding\.pry'
 
-RESULT=$(grep -i $PATTERN $DIFF_SEARCH)
+RESULT=$(grep -v '#' $DIFF_SEARCH | grep -i $PATTERN)
 
 if [ "$RESULT" ]; then
   echo "Exit build; found binding.pry in this commit."
